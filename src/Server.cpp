90a54b378f46c5b501a95b9a3259a95f81cbb22d
Mohnaa Ghosh <mohnaag@gmail.com>
2024-09-27 14:12:50 +0530

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 
-bool match_pattern(const std::string& input_line, const std::string& pattern) {
-    if (pattern.length() == 1) {
-        return input_line.find(pattern) != std::string::npos;
-    }
-    else {
-        throw std::runtime_error("Unhandled pattern " + pattern);
-    }
+bool match_pattern(const std::string &input_line, const std::string &pattern) {
+  if (pattern.length() == 1) {
+    return input_line.find(pattern) != std::string::npos;
+  } else if (pattern == "\\d") {
+    return input_line.find_first_of("123456890") != std::string::npos;
+  } else {
+    throw std::runtime_error("Unhandled pattern " + pattern);}
 }
 
 int main(int argc, char* argv[]) {
