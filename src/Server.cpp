6ef840a48cb274d7a47fcc9df1057df1412882a2
Mohnaa Ghosh <mohnaag@gmail.com>
2024-09-27 14:47:38 +0530

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
 #include <string>
-#include <bits/stdc++.h>
+#include <cctype>
 using namespace std;
-
-bool match_digits(const std::string& inputs, const std::string& pattern) {
-    for (auto input : inputs) {
-        if (isdigit(input)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool match_alphanum(const std::string& inputs, const std::string& pattern) {
-    for (auto c : inputs) {
-        if (isalnum(c)) {
-            return true;
-        }
-    }
-    return false;
-}
 
 bool match_group(const string& input_line, const string& pattern) {
     return input_line.find_first_of(pattern) != string::npos;
@@ -34,12 +16,12 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
         if (isdigit(input_line[0])) {
             return match_pattern(input_line.substr(1), pattern.substr(2));
         }
-        return match_pattern(input_line.substr(1), pattern);
+        return false; // Return false if the current character is not a digit
     } else if (pattern.substr(0, 2) == "\\w") {
         if (isalnum(input_line[0])) {
             return match_pattern(input_line.substr(1), pattern.substr(2));
         }
-        return match_pattern(input_line.substr(1), pattern);
+        return false; // Return false if the current character is not alphanumeric
     } else if (pattern[0] == '[') {
         auto first = pattern.find(']');
         bool neg = pattern[1] == '^';
@@ -59,32 +41,45 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern[0] == input_line[0]) {
         return match_pattern(input_line.substr(1), pattern.substr(1));
     }
-    return match_pattern(input_line.substr(1), pattern);
+    return false;
 }
 
-bool match_patterns(string input_line, string pattern) {
-    do {
-        if (match_pattern(input_line, pattern)) {
+bool match_patterns(const std::string& input_line, const std::string& pattern) {
+    size_t i = 0;
+    while (i < input_line.size()) {
+        if (match_pattern(input_line.substr(i), pattern)) {
             return true;
         }
-        input_line = input_line.substr(1); // move 1 step further
-    } while (input_line != "");
+        i++;
+    }
     return false;
 }
 
 int main(int argc, char* argv[]) {
-    string input_line;
-    std::getline(std::cin, input_line);
+    if (argc != 3) {
+        cerr << "Usage: ./your_program -E <pattern>" << endl;
+        return 1;
+    }
 
-    string pattern = "\\d"; // Example pattern, you can change it as needed
+    string flag = argv[1];
+    string pattern = argv[2];
+
+    if (flag != "-E") {
+        cerr << "Expected '-E' flag" << endl;
+        return 1;
+    }
+
+    string input_line;
+    getline(cin, input_line);
+
     try {
         if (match_patterns(input_line, pattern)) {
-            return 0;
+            return 0;  // Pattern matched
         } else {
-            return 1;
+            return 1;  // Pattern did not match
         }
     } catch (const std::runtime_error& e) {
-        std::cerr << e.what() << std::endl;
+        cerr << e.what() << endl;
         return 1;
     }
 }
